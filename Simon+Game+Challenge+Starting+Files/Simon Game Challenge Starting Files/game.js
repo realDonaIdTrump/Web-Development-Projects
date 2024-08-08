9f8532b797f8559792f818d5c62014c6b2d01b54
hideonbus <32107008+hideonbus@users.noreply.github.com>
2024-08-08 23:59:45 +0800

    games js created

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,4 +1,10 @@
-$(".btn").click(function(){
-    var buttonSelect = $(this).attr("id");
-    console.log(buttonSelect);
-})
+const buttonColours = new Array("red", "blue", "green", "yellow");
+
+var randomChosenColour = buttonColours[nextSequence()];
+
+console.log(randomChosenColour);
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4);
+    return randomNumber;
+}
